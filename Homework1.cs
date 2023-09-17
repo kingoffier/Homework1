83f220635f8aef3902c93aff3ffa9d93ae2e4b22
kingoffier <144258616+kingoffier@users.noreply.github.com>
2023-09-17 20:02:30 +0500

    Add files via upload

---
+++ Homework1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты точки A:");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты точки B:");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine($"Длина отрезка AB = {distance:F3}");
+            Console.ReadKey();
+        }
+    }
+}
