99c7b905570c45e6e6474a848d7d079a51b9078e
kingoffier <144258616+kingoffier@users.noreply.github.com>
2023-09-17 19:38:52 +0500

    Add files via upload

---
+++ Homework2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = Convert.ToInt32(Console.ReadLine());
+            int second = Convert.ToInt32(Console.ReadLine());
+            (first, second) = (second, first);
+            Console.WriteLine("Первое число = " + first);
+            Console.WriteLine("Второе число = " + second);
+            Console.ReadKey();
+
+        }
+    }
+}
